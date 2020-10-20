6bd062ef1366ff0f77aec77313b118dfdf49099f
Raphael Mendes <raphaelmendes18@gmail.com>
2020-10-20 08:52:13 -0300

    adding edge class

--- components/edge.py
+++ components/edge.py
@@ -1,4 +1,6 @@
 class Edge:
 
-    def __init__(self, **kwargs):
+    def __init__(self, start, end, **kwargs):
+        self.start = start
+        self.end = end
         self.__dict__.update(kwargs)
--- components/graph.py
+++ components/graph.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-
+from components.edge import Edge
 class Graph:
 
     def __init__(self, graph_matrix=None, num_nodes=None, num_connections=None):
@@ -22,33 +22,20 @@ class Graph:
         assert num_connections >= num_nodes - 1, 'Invalid number of connections, need to be at least num_connections >= num_nodes - 1'
         assert num_connections <= (num_nodes*(num_nodes-1))/2, 'Invalid number of connections, need to be maximum of num_nodes*(num_nodes-1)/2 (bidirectional graph)'
 
-        self.graph = np.random.randint(low=min_weight, high=max_weight, size=(num_nodes, num_nodes), dtype=int).tolist()
+        self.graph = np.empty(shape=(num_nodes,num_nodes), dtype=object)
         connections = self.get_number_of_connections()
 
-        while (connections != num_connections):
-            if connections > num_connections:
-                a = random.randint(0,self.num_nodes)
-                b = a
-                while((a == b) | (self.is_connected(a, b) == True)):
-                    b = random.randint(0,self.num_nodes)
-                temp_value = self.graph[a,b]
-                self.remove_connection(a,b)
-                if self.is_fully_connected():
-                    connections -= 1
-                else:
-
-
     def add_connection(self, a, b, **kwargs):
-        self.graph[a,b] = Edge(kwargs)
-        self.graph[b,a] = Edge(kwargs)
+        self.graph[a,b] = Edge(a, b, **kwargs)
+        self.graph[b,a] = Edge(b, a, **kwargs)
 
     def DepthFirstSearch(self, v, visited): 
         # Mark the current node as visited
         visited[v] = True
-        for vertix, weight in enumerate(self.graph[v,:]):
-            if weight != 0:
-                if visited[vertix]==False: 
-                    self.DepthFirstSearch(vertix, visited)
+        for idx, edge in enumerate(self.graph[v,:]):
+            if edge:
+                if visited[idx]==False: 
+                    self.DepthFirstSearch(idx, visited)
 
     def is_fully_connected(self):
         visited = [False]*self.num_nodes
@@ -58,18 +45,18 @@ class Graph:
         return True
 
     def is_connected(self, a, b):
-        return self.graph[a,b] != 0
+        return self.graph[a,b] != None
 
     def remove_connection(self, a, b):
-        self.graph[a,b] = 0
-        self.graph[b,a] = 0
+        self.graph[a,b] = None
+        self.graph[b,a] = None
         
 
     def get_number_of_connections(self):
         connections = 0
-        for i in graph.shape[0]:
-            for j in graph.shape[1]:
-                if graph[i,j] != 0:
+        for i in self.graph.shape[0]:
+            for j in self.graph.shape[1]:
+                if self.graph[i,j] != None:
                     connections += 1
         return int(connections/2)
 
