a4815ec5fec31f4a75faeb727a5586ebbcdbaf80
Raphael Mendes <raphaelmendes18@gmail.com>
2020-11-05 10:02:28 -0300

    comments

--- components/crossover.py
+++ components/crossover.py
@@ -12,7 +12,6 @@ class BuenoOliveiraCrossover(Crossover):
     def crossover(self, partner1, partner2):
         
         # find edges in common
-        # print(f'Parents have Cycle: {partner1.multicast_tree.has_cycle()} - {partner2.multicast_tree.has_cycle()}')
         p1_edges = partner1.multicast_tree.edges()
         edges_in_common = []
 
@@ -31,7 +30,7 @@ class BuenoOliveiraCrossover(Crossover):
         if self.root_node not in aux_tree.nodes:
             aux_tree.add_node(self.root_node)
 
-        while nx.is_connected(aux_tree) == False: # 4. Repeat process until graph is connected
+        while nx.is_connected(aux_tree) == False: # 0. Repeat process until graph is connected
             available_edges = []
             for node in aux_tree.nodes:
                 # 1. Get all neighbor edges not yet in aux_tree
@@ -47,11 +46,12 @@ class BuenoOliveiraCrossover(Crossover):
                     pass
             # 3. Randomly pick one edge
             new_edge = random.choice(available_edges)
+            # 4. Check if new edge adds a cycle
             try:
                 test_cycle = aux_tree.copy()
                 test_cycle.add_edge(new_edge[0],new_edge[1])
                 cycles = nx.find_cycle(test_cycle)
-            except:
+            except: # will raise if no cycles are found
                 aux_tree.add_edge(new_edge[0],new_edge[1])
         # 5. Rebuild aux_tree 
         child = nx.Graph()
@@ -59,6 +59,7 @@ class BuenoOliveiraCrossover(Crossover):
             edge_obj = Edge(edge[0],edge[1], **self.graph.graph.edges[(edge[0],edge[1])])
             child.add_edge(edge_obj.start, edge_obj.end, **edge_obj.as_dict())   
         
+        # 6. Prune Tree
         child_tree = MulticastRoute(Graph(child), self.root_node, self.destination_nodes, build_tree=False)
         # print(f'Child has Cycle: {child_tree.has_cycle()}')
         return child_tree
